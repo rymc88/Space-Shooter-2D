a624312632844b57695658772f050ec44475202a
rymc88 <64868815+rymc88@users.noreply.github.com>
2021-03-23 21:41:58 -0400

    Player Movement
    Player movement is controlled using the arrow or ASWD keys. Boundaries have been place on the player so they cannot leave the screen or go above 0 on the Y.

---
+++ Space Shooter 2D/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+        [SerializeField] private float _speed = 5.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CalculateMovement();
+        }
+
+        void CalculateMovement()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 velocity = new Vector3(horizontalInput, verticalInput, 0) * _speed;
+
+            transform.Translate(velocity * Time.deltaTime);
+
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9, 9), Mathf.Clamp(transform.position.y, -3.5f, 0), 0);
+        }
+  
+}
