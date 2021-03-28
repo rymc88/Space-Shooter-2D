9a6824660a9d49a5b755c884ff34f83d47053b0f
rymc88 <64868815+rymc88@users.noreply.github.com>
2021-03-28 12:24:31 -0400

    Shoot Mechanic
    When the player press the Space Bar a laser will be create from the player with an offset and move up the screen. Once off the screen it will be destory. A firerate has been included.

--- Space Shooter 2D/Assets/Scripts/Player.cs
+++ Space Shooter 2D/Assets/Scripts/Player.cs
@@ -4,29 +4,69 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-        [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _speed = 5.5f;
+    [SerializeField] private string _playersInitials;
+    public int score;
+    [SerializeField] private bool _powerupActive;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private float _fireRate = 0.5f;
+    private float _fireTime = -1f;
+    
 
-        // Start is called before the first frame update
-        void Start()
+   // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+   // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _fireTime)
         {
-            transform.position = new Vector3(0, 0, 0);
+            FireLaser();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            CalculateMovement();
-        }
+    }
 
-        void CalculateMovement()
-        {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 velocity = new Vector3(horizontalInput, verticalInput, 0) * _speed;
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(horizontalInput, verticalInput, 0) * _speed;
 
-            transform.Translate(velocity * Time.deltaTime);
+        transform.Translate(velocity * Time.deltaTime);
 
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9, 9), Mathf.Clamp(transform.position.y, -3.5f, 0), 0);
-        }
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9, 9), Mathf.Clamp(transform.position.y, -3.8f, 5.8f), 0);
+
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9, 9), Mathf.Clamp(transform.position.y, -3.5f, 0), 0);
+
+        /*if(transform.position.x < -10)
+            {
+                transform.position = new Vector3(10, transform.position.y, 0);
+            }
+        else if (transform.position.x > 10)
+            {
+                transform.position = new Vector3(-10, transform.position.y, 0);
+            }
+        else if(transform.position.y > 7)
+            {
+                transform.position = new Vector3(transform.position.x, -5, 0);
+            }
+        else if (transform.position.y < -5)
+            {
+                transform.position = new Vector3(transform.position.x, 7, 0);
+            }*/
+   }
+
+    void FireLaser()
+    {
+        _fireTime = Time.time + _fireRate;
+        var offset = new Vector3(0, 1, 0);
+        Instantiate(_laserPrefab, transform.position + offset, Quaternion.identity);
+    }
+
   
 }
